a43625cac147df1d5659c35229cb4a302bdc1a36
Paula Andujar <paulandujarzambrano@gmail.com>
2024-02-26 09:59:55 +0100

    Reapply "Primer commit"
    This reverts commit 9c816659cf116e9874e278839aaf42b05025a4b0.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppComponent } from './app.component';
+
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
-
-import { AppComponent } from './app.component';
+import { DbzModule } from './dbz/dbz.module';
 
 
 
@@ -18,6 +19,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     CounterModule,
     HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
